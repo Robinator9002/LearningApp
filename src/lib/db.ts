80f59860ef951c62758d9545d6802c68819fbc67
Robinator9002 <r.schumi09@gmail.com>
2025-08-27 10:43:18 +0200

    refactor (lib): Fix type only import in db.ts

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,4 +1,4 @@
-import Dexie, { Table } from 'dexie';
+import Dexie, { type Table } from 'dexie';
 
 // We will define these interfaces in the /types folder later
 // import { IUser, ICourse, IProgressLog } from '../types';
