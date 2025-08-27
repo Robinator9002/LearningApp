d338f1adce245361ae0e884d52ba78364b09cbf5
Robinator9002 <r.schumi09@gmail.com>
2025-08-27 10:56:27 +0200

    feat (pages): Create placeholders for the main pages

---
+++ src/pages/AdminDashboardPage.tsx
@@ -0,0 +1,12 @@
+// src/pages/AdminDashboardPage.tsx
+import React from 'react';
+
+const AdminDashboardPage: React.FC = () => {
+    return (
+        <div>
+            <h1>Admin Dashboard</h1>
+        </div>
+    );
+};
+
+export default AdminDashboardPage;
---
+++ src/pages/UserSelectionPage.tsx
@@ -0,0 +1,13 @@
+// src/pages/UserSelectionPage.tsx
+import React from 'react';
+
+const UserSelectionPage: React.FC = () => {
+    return (
+        <div>
+            <h1>Select User Profile</h1>
+            {/* User buttons will be rendered here */}
+        </div>
+    );
+};
+
+export default UserSelectionPage;
